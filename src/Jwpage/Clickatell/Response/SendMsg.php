213abb0bfc0122e16e1900b70effac9f6277060d
Johnson Page <jwpage@gmail.com>
2013-01-05 17:54:36 +1000

    Add better multiple response and tests

--- src/Jwpage/Clickatell/Response/SendMsg.php
+++ src/Jwpage/Clickatell/Response/SendMsg.php
@@ -2,13 +2,40 @@
 
 namespace Jwpage\Clickatell\Response;
 
+use Jwpage\Clickatell\Error;
+
 class SendMsg extends AbstractResponse
 {
+    public function isSuccessful()
+    {
+        foreach ($this->parsedResponse as $line) {
+            if ($line[1] == 'ERR') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // alias for getMessageIds
     public function getMessageId()
     {
-        return $this->isSuccessful() 
-            ? trim($this->parsedResponse[2])
-            : false;
+        return $this->getMessageIds();
+    }
+
+    public function getMessageIds()
+    {
+        $toNumbers = explode(',', $this->request->getPostField('to'));
+        $result = array();
+        foreach ($this->parsedResponse as $key => $line) {
+            if ($line[1] == 'ERR') {
+                $item = new Error($line[2]);
+            } else {
+                preg_match('/^(.*?)\s/', $line[2], $matches);
+                $item = $matches[1];
+            }
+            $result[$toNumbers[$key]] = $item;
+        }
+        return $result;
     }
 
 }
