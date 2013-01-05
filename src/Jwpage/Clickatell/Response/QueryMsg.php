afa1f012e9ab7b671fa4fac8bc7fbc2818a528df
Johnson Page <jwpage@gmail.com>
2013-01-05 18:14:26 +1000

    Return false if not successful

--- src/Jwpage/Clickatell/Response/QueryMsg.php
+++ src/Jwpage/Clickatell/Response/QueryMsg.php
@@ -6,6 +6,10 @@ class QueryMsg extends AbstractResponse
 {
     public function getStatus()
     {
+        if (!$this->isSuccessful()) {
+            return false;
+        }
+
         preg_match('/Status: (.*?)$/', $this->parsedResponse[0][2], $matches);
         return $matches[1];
     }
