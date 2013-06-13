756facff0d7a206689ed33d2faa8864a2ef40a04
Johnson Page <jwpage@gmail.com>
2013-06-13 13:52:57 +1000

    Enable deprecation warnings in Guzzle 3.7

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
 
 require __DIR__.'/../vendor/autoload.php';
+error_reporting(-1);
 Guzzle\Tests\GuzzleTestCase::setMockBasePath(__DIR__.'/mock');
+Guzzle\Common\Version::$emitWarnings = true;
+
